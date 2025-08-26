d8217cddb9a7fdbc10f852cd65721a7502b981cd
Hergen Engelhardt <engelhardt.hergen@gmail.com>
2025-08-26 19:00:47 +0200

    updated order app

--- order_app/api/serializers.py
+++ order_app/api/serializers.py
@@ -7,31 +7,34 @@ from offer_app.models import OfferDetail
 class OrderSerializer(serializers.ModelSerializer):
     """
     Serializer for order objects.
-    
+
     Args:
-        id: Order ID
-        customer_user: Customer user ID
-        business_user: Business user ID
-        title: Order title
-        revisions: Number of revisions
-        delivery_time_in_days: Delivery time in days
-        price: Order price
-        features: List of features
-        offer_type: Offer type
-        status: Order status
-        created_at: Creation timestamp
-        updated_at: Update timestamp
-        
+        id (int): Order ID
+        customer_user (User): Customer user
+        business_user (User): Business user
+        title (str): Order title
+        revisions (int): Number of revisions
+        delivery_time_in_days (int): Delivery time in days
+        price (Decimal): Order price
+        features (list): List of features
+        offer_type (str): Offer type
+        status (str): Order status
+        created_at (datetime): Creation timestamp
+        updated_at (datetime): Update timestamp
+
     Returns:
         Order: Serialized order data
+
+    Raises:
+        None
     """
-    
+
     class Meta:
         model = Order
         fields = [
             'id',
             'customer_user',
-            'business_user', 
+            'business_user',
             'title',
             'revisions',
             'delivery_time_in_days',
@@ -48,7 +51,7 @@ class OrderSerializer(serializers.ModelSerializer):
             'business_user',
             'title',
             'revisions',
-            'delivery_time_in_days', 
+            'delivery_time_in_days',
             'price',
             'features',
             'offer_type',
@@ -60,26 +63,26 @@ class OrderSerializer(serializers.ModelSerializer):
 class OrderCreateSerializer(serializers.ModelSerializer):
     """
     Serializer for creating orders from offer details.
-    
+
     Args:
-        offer_detail_id: ID of the offer detail to create order from
-        
+        offer_detail_id (int): ID of the offer detail to create order from
+
     Returns:
         Order: Created order instance
-        
+
     Raises:
         ValidationError: If offer detail not found or invalid
     """
-    
+
     offer_detail_id = serializers.IntegerField(write_only=True)
-    
+
     class Meta:
         model = Order
         fields = [
             'id',
             'offer_detail_id',
             'customer_user',
-            'business_user', 
+            'business_user',
             'title',
             'revisions',
             'delivery_time_in_days',
@@ -96,24 +99,24 @@ class OrderCreateSerializer(serializers.ModelSerializer):
             'business_user',
             'title',
             'revisions',
-            'delivery_time_in_days', 
+            'delivery_time_in_days',
             'price',
             'features',
             'offer_type',
             'created_at',
             'updated_at'
         ]
-    
+
     def validate_offer_detail_id(self, value):
         """
         Validate offer detail exists.
-        
+
         Args:
-            value: Offer detail ID
-            
+            value (int): Offer detail ID
+
         Returns:
             int: Validated offer detail ID
-            
+
         Raises:
             ValidationError: If offer detail not found
         """
@@ -122,22 +125,41 @@ class OrderCreateSerializer(serializers.ModelSerializer):
         except OfferDetail.DoesNotExist:
             raise serializers.ValidationError("Offer detail not found.")
         return value
-    
+
     def create(self, validated_data):
         """
         Create order from offer detail.
-        
+
         Args:
-            validated_data: Validated data containing offer_detail_id
-            
+            validated_data (dict): Validated data containing offer_detail_id
+
         Returns:
             Order: Created order instance
+
+        Raises:
+            None
         """
         offer_detail_id = validated_data.pop('offer_detail_id')
         offer_detail = OfferDetail.objects.get(id=offer_detail_id)
-        
-        order = Order.objects.create(
-            customer_user=self.context['request'].user,
+        order = self._create_order_from_detail(offer_detail, self.context['request'].user)
+        return order
+
+    def _create_order_from_detail(self, offer_detail, customer_user):
+        """
+        Create order instance from offer detail.
+
+        Args:
+            offer_detail (OfferDetail): Offer detail instance
+            customer_user (User): Customer user
+
+        Returns:
+            Order: Created order instance
+
+        Raises:
+            None
+        """
+        return Order.objects.create(
+            customer_user=customer_user,
             business_user=offer_detail.offer.user,
             title=offer_detail.title,
             revisions=offer_detail.revisions,
@@ -146,35 +168,36 @@ class OrderCreateSerializer(serializers.ModelSerializer):
             features=offer_detail.features,
             offer_type=offer_detail.offer_type
         )
-        
-        return order
 
 
 class OrderStatusUpdateSerializer(serializers.ModelSerializer):
     """
     Serializer for updating order status.
-    
+
     Args:
-        status: New order status
-        
+        status (str): New order status
+
     Returns:
         Order: Updated order instance
+
+    Raises:
+        ValidationError: If status is invalid
     """
-    
+
     class Meta:
         model = Order
         fields = ['status']
-    
+
     def validate_status(self, value):
         """
         Validate status value.
-        
+
         Args:
-            value: Status value to validate
-            
+            value (str): Status value to validate
+
         Returns:
             str: Validated status value
-            
+
         Raises:
             ValidationError: If status is invalid
         """
@@ -187,26 +210,32 @@ class OrderStatusUpdateSerializer(serializers.ModelSerializer):
 class OrderCountSerializer(serializers.Serializer):
     """
     Serializer for order count responses.
-    
+
     Args:
-        order_count: Number of orders
-        
+        order_count (int): Number of orders
+
     Returns:
         dict: Order count data
+
+    Raises:
+        None
     """
-    
+
     order_count = serializers.IntegerField()
 
 
 class CompletedOrderCountSerializer(serializers.Serializer):
     """
     Serializer for completed order count responses.
-    
+
     Args:
-        completed_order_count: Number of completed orders
-        
+        completed_order_count (int): Number of completed orders
+
     Returns:
         dict: Completed order count data
+
+    Raises:
+        None
     """
-    
+
     completed_order_count = serializers.IntegerField()
